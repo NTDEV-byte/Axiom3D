61fbd094396c25df953810fefedd738b343ce741
NTDEV-byte <nassim.t998@gmail.com>
2022-08-16 23:11:27 +0200

    Window Timer Completed

--- src/core/Window.java
+++ src/core/Window.java
@@ -1,6 +1,7 @@
 package core;
 
 import org.lwjgl.Sys;
+import org.lwjgl.opengl.Display;
 
 import javax.swing.*;
 
@@ -19,24 +20,29 @@ public class Window {
 
         public static void createWindow(){
 
-
         }
 
 
         private static double update(){}
+
 
         private static double getTimeInMS(){
                 return Sys.getTime() * 1000 / Sys.getTimerResolution();
         }
 
         private static void activateTimer(){
-
+                now = getTimeInMS();
+                delta = (now - lastTime) / 1000; // delta is in second's
+                lastTime = now;
         }
 
-        private static double limitFPS(int maxFPS){
-
+        private static void limitFPS(int maxFPS){
+                Display.sync(maxFPS);
         }
 
+        public static double getElapsedTime(){ // the elapsed time between two frames
+                return delta;
+        }
 
 
 }
