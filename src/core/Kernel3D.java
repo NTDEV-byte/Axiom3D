e8e67b5d8014a0dda11b9d5f82a0afb1fa16d654
NTDEV-byte <nassim.t998@gmail.com>
2022-08-16 22:59:14 +0200

    Kernel3D Interface

--- src/core/Kernel3D.java
+++ src/core/Kernel3D.java
@@ -1,15 +1,32 @@
 package core;
 
-public class Kernel3D implements IProgram{
+public class Kernel3D implements IProgram ,Runnable{
+
+    private boolean running;
+    private Thread thread;
 
 
     @Override
-    public void start() {
-
-    }
+    public void start() {}
+    @Override
+    public void stop() {}
 
     @Override
-    public void stop() {
+    public void run() {
+        this.initialize();
+        while(running){
+            update();
+            render();
+        }
+    }
+    private void initialize(){}
+    private void update(){}
+    private void render(){}
+
+
+
+    public static void main(String args[]){
 
     }
+
 }
