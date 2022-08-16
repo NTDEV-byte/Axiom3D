692e388ffe25a95e3ab2ebd7f43ed73dd8806e10
NTDEV-byte <nassim.t998@gmail.com>
2022-08-16 23:10:10 +0200

    Window Interface timer

--- src/core/Window.java
+++ src/core/Window.java
@@ -1,14 +1,39 @@
 package core;
 
+import org.lwjgl.Sys;
+
+import javax.swing.*;
+
 public class Window {
 
         public static final int WIDTH = 1280;
         public static final int HEIGHT = 720;
+        public static final int MAX_FPS = 120;
         public static final String TITLE = "Kernel3D";
+
+        private static double lastTime = getTimeInMS();
+        private static double now;
+        private static double delta;
 
         private Window(){}
 
         public static void createWindow(){
+
+
+        }
+
+
+        private static double update(){}
+
+        private static double getTimeInMS(){
+                return Sys.getTime() * 1000 / Sys.getTimerResolution();
+        }
+
+        private static void activateTimer(){
+
+        }
+
+        private static double limitFPS(int maxFPS){
 
         }
 
