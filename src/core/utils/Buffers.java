9f95a71c53caae24a34a551297d233c0ee353100
NTDEV-byte <nassim.t998@gmail.com>
2022-08-16 23:26:08 +0200

    BuffersTools creating int buffer from data

--- src/core/utils/Buffers.java
+++ src/core/utils/Buffers.java
@@ -1,6 +1,8 @@
 package core.utils;
 
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
 
 public class Buffers {
 
@@ -10,6 +12,15 @@ public class Buffers {
         public static ByteBuffer createByteBuffer(byte data[]){
 
                 ByteBuffer bufferData = ByteBuffer.allocateDirect(data.length);
+                bufferData.put(data);
+                bufferData.flip();
+
+                return bufferData;
+        }
+
+        public static IntBuffer createInBuffer(int data[]){
+
+                IntBuffer bufferData = ByteBuffer.allocateDirect(data.length << 4).order(ByteOrder.nativeOrder()).asIntBuffer();
                 bufferData.put(data);
                 bufferData.flip();
 
