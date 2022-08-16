a241766098662284d45a265d23501b5cc8dff3f1
NTDEV-byte <nassim.t998@gmail.com>
2022-08-16 23:41:44 +0200

    Shader creation with only vertex & fragment stages

--- src/core/utils/shaders/Shader.java
+++ src/core/utils/shaders/Shader.java
@@ -1,5 +1,9 @@
 package core.utils.shaders;
 
+import core.utils.FileLoader;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL20;
+
 public class Shader implements IShader{
 
         private int id;
@@ -55,7 +59,36 @@ public class Shader implements IShader{
             return -1;
         }
 
-        private int createShaderVFProgram(String vertexPath,String fragmentPath){}
+        private int createShaderVFProgram(String vertexPath,String fragmentPath){
+            String vertexContent = FileLoader.loadFile(vertexPath);
+            String fragmentContent = FileLoader.loadFile(fragmentPath);
+
+            int programID = GL20.glCreateProgram();
+            int vertexID = GL20.glCreateShader(GL20.GL_VERTEX_SHADER);
+            int fragmentID = GL20.glCreateShader(GL20.GL_FRAGMENT_SHADER);
+
+            GL20.glShaderSource(vertexID , vertexContent);
+            GL20.glCompileShader(vertexID);
+            if(GL20.glGetShaderi(vertexID, GL20.GL_COMPILE_STATUS) == GL11.GL_FALSE){
+                System.err.println("Erreur lors de la compilation du vertexShader: "+vertexPath);
+            }
+            GL20.glShaderSource(fragmentID , fragmentContent);
+            GL20.glCompileShader(fragmentID);
+            if(GL20.glGetShaderi(fragmentID, GL20.GL_COMPILE_STATUS) == GL11.GL_FALSE){
+                System.err.println("Erreur lors de la compilation du fragmentShader: "+fragmentPath);
+            }
+
+            GL20.glAttachShader(programID,vertexID);
+            GL20.glAttachShader(programID,fragmentID);
+
+            GL20.glLinkProgram(programID);
+            GL20.glValidateProgram(programID);
+
+            GL20.glDeleteShader(vertexID);
+            GL20.glDeleteShader(fragmentID);
+
+            return programID;
+        }
 
         private int createShaderVGFProgram(String vertexPath,String geometryPath,String fragmentPath){}
 
