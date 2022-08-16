811c43a841488c418993a084d378516a06a78cdc
NTDEV-byte <nassim.t998@gmail.com>
2022-08-16 23:04:24 +0200

    Window Interface

---
+++ src/core/Window.java
@@ -0,0 +1,17 @@
+package core;
+
+public class Window {
+
+        public static final int WIDTH = 1280;
+        public static final int HEIGHT = 720;
+        public static final String TITLE = "Kernel3D";
+
+        private Window(){}
+
+        public static void createWindow(){
+
+        }
+
+
+
+}
