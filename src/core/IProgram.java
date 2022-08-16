f26502d80b1d61aa25dd47e487c56cd5203cf838
NTDEV-byte <nassim.t998@gmail.com>
2022-08-16 22:56:57 +0200

    IProgram

---
+++ src/core/IProgram.java
@@ -0,0 +1,7 @@
+package core;
+
+public interface IProgram {
+
+            public void start();
+            public void stop();
+}
--- src/core/Kernel3D.java
+++ src/core/Kernel3D.java
@@ -1,4 +1,15 @@
 package core;
 
-public class Kernel3D {
+public class Kernel3D implements IProgram{
+
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
 }
