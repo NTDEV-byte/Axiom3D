e3f0785f3fb3ce4e77567f28d01c0bed44b3aeb4
NTDEV-byte <nassim.t998@gmail.com>
2022-08-17 13:18:14 +0200

    Basic VertexShader

---
+++ resources/shaders/basicVS.glsl
@@ -0,0 +1,16 @@
+#version 400
+
+layout (location = 0) in vec3 positions;
+layout (location = 1) in vec2 textures;
+layout (location = 2) in vec3 normals;
+
+uniform mat4 modelMatrix = mat4(1.0);
+uniform mat4 viewMatrix = mat4(1.0);
+uniform mat4 projectionMatrix = mat4(1.0);
+
+
+
+void main() {
+
+        gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(positions);
+}
