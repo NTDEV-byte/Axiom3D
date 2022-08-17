755ecd85d6d8eb6d57bf6838af2fe9ebd187364f
NTDEV-byte <nassim.t998@gmail.com>
2022-08-17 14:38:10 +0200

    getEyeSpace

--- src/core/scene/camera/Camera.java
+++ src/core/scene/camera/Camera.java
@@ -1,25 +1,42 @@
 package core.scene.camera;
 
+import core.scene.Scene;
 import org.lwjgl.util.vector.Matrix4f;
 import org.lwjgl.util.vector.Vector3f;
 
+import static core.scene.Scene.*;
 
-public class Camera implements ICamera {
+
+public abstract class Camera implements ICamera {
 
     protected Matrix4f viewMatrix;
     protected Vector3f position;
     protected Vector3f rotation;
     protected Vector3f invPosition;
 
-    public Camera(Vector3f position){
+    protected Camera(Vector3f position){
         this.position = position;
         this.rotation = new Vector3f();
         this.viewMatrix = new Matrix4f();
         this.invPosition = new Vector3f();
     }
 
-    @Override
+    public void update(){
+        this.keyBoardInput();
+        this.mouseInput();
+    }
+
+    protected abstract void keyBoardInput();
+    protected abstract void mouseInput();
+
+    @Override // or let me see through your eyes ^^
     public Matrix4f getEyeSpace() {
-        return null;
+        viewMatrix.setIdentity();
+        viewMatrix.rotate((float)(Math.toRadians(rotation.x)) , X_AXIS);
+        viewMatrix.rotate((float)(Math.toRadians(rotation.y)) , Y_AXIS);
+        viewMatrix.rotate((float)(Math.toRadians(rotation.z)) , Z_AXIS);
+        invPosition.set(-position.x , -position.y , -position.z);
+        viewMatrix.translate(invPosition);
+        return viewMatrix;
     }
 }
