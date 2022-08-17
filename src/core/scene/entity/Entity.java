77f76f5a9506902c322a69596bbf0869248f89d8
NTDEV-byte <nassim.t998@gmail.com>
2022-08-17 14:16:57 +0200

    Entity abstract class

--- src/core/scene/entity/Entity.java
+++ src/core/scene/entity/Entity.java
@@ -1,4 +1,54 @@
 package core.scene.entity;
 
-public class Entity {
+import core.scene.camera.ICamera;
+import core.utils.shaders.Shader;
+import core.utils.textures.Texture;
+import core.utils.vaos.VertexArray;
+import org.lwjgl.util.vector.Matrix4f;
+import org.lwjgl.util.vector.Vector3f;
+
+public abstract class Entity implements IEntity {
+
+
+    protected VertexArray mesh;
+    protected Shader shader;
+    protected Texture texture;
+
+    protected Vector3f position;
+    protected Vector3f rotation;
+    protected Vector3f scale;
+
+    protected Matrix4f modelMatrix;
+
+
+    // init
+    private void initDefault(){}
+    private void initScaled(){}
+
+
+    // entity related behaviour
+    protected abstract void update();
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void rotate(int pitch, int yaw, int roll) {
+
+    }
+
+    @Override
+    public void move(int dx, int dy, int dz) {
+
+    }
+
+    // shader data loading
+
+    protected void loadModelMatrix(){}
+
+    protected void loadViewMatrix(ICamera camera){}
+
+    protected void loadProjectionMatrix(){}
 }
