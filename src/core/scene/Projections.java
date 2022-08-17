70b16bfe5a3b0fdbf471c3c5fac6ed6c851d01b2
NTDEV-byte <nassim.t998@gmail.com>
2022-08-17 15:10:56 +0200

    Perspective Projection loaded

--- src/core/scene/Projections.java
+++
@@ -1,6 +0,0 @@
-package core.scene;
-
-public class Projections {
-
-        private Projections(){}
-}
--- src/shapes/d2/Rectangle.java
+++ src/shapes/d2/Rectangle.java
@@ -5,12 +5,12 @@ import core.scene.entity.Entity;
 import core.utils.shaders.Shader;
 import core.utils.vaos.Vertex;
 import core.utils.vaos.VertexArray;
+import org.lwjgl.util.vector.Matrix4f;
 import org.lwjgl.util.vector.Vector3f;
 
 import java.util.Arrays;
 
 public class Rectangle extends Entity {
-
 
     private static VertexArray mesh = new VertexArray(
             Arrays.asList(new Vertex[]{
@@ -21,15 +21,16 @@ public class Rectangle extends Entity {
             }),
             Arrays.asList(new Integer[]{0,1,2 , 2 , 1 , 3}));
 
-
     public Rectangle(Vector3f position) {
         super(mesh, new Shader("resources/shaders/basicVS.glsl" , "resources/shaders/basicFS.glsl"), position);
     }
-
     @Override
     public void update() {
         super.loadModelMatrix();
         super.loadViewMatrix(Scene.MAIN_CAMERA);
         super.rotate(0.0f,6f,0);
     }
+
+
+
 }
