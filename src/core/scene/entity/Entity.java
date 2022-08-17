160f2635f9135fd8bf54bae695dc40ab9b6735d6
NTDEV-byte <nassim.t998@gmail.com>
2022-08-17 14:22:39 +0200

    Init default & scaled

--- src/core/scene/entity/Entity.java
+++ src/core/scene/entity/Entity.java
@@ -9,51 +9,60 @@ import org.lwjgl.util.vector.Vector3f;
 
 public abstract class Entity implements IEntity {
 
-
     protected VertexArray mesh;
     protected Shader shader;
     protected Texture texture;
-
     protected Vector3f position;
     protected Vector3f rotation;
     protected Vector3f scale;
 
     protected Matrix4f modelMatrix;
 
-
     public Entity(VertexArray mesh, Shader shader, Vector3f position) {
         this.mesh = mesh;
         this.shader = shader;
         this.position = position;
+        this.initDefault();
     }
-
-    public Entity(VertexArray mesh, Shader shader, Vector3f position, float scalar) {
-        this(mesh,shader,position);
+    public Entity(VertexArray mesh, Shader shader, Vector3f position,float scalar) {
+        this.mesh = mesh;
+        this.shader = shader;
+        this.position = position;
         this.scale = new Vector3f(scalar,scalar,scalar);
+        this.initScaled();
     }
-
     public Entity(VertexArray mesh, Shader shader, Vector3f position,Texture texture) {
         this.mesh = mesh;
         this.shader = shader;
         this.position = position;
         this.texture = texture;
+        this.initDefault();
     }
     public Entity(VertexArray mesh, Shader shader,Vector3f position,Texture texture,float scalar) {
-        this(mesh,shader,position,texture);
+        this.mesh = mesh;
+        this.shader = shader;
+        this.position = position;
+        this.texture = texture;
         this.scale = new Vector3f(scalar,scalar,scalar);
+        this.initScaled();
     }
 
     // init
-    private void initDefault(){}
-    private void initScaled(){}
-
+    private void initDefault(){
+        this.rotation = new Vector3f();
+        this.modelMatrix = new Matrix4f();
+        this.scale = new Vector3f(1.0f , 1.0f , 1.0f);
+    }
+    private void initScaled(){
+        this.rotation = new Vector3f();
+        this.modelMatrix = new Matrix4f();
+    }
 
     // entity related behaviour
     protected abstract void update();
 
     @Override
     public void render() {
-
     }
 
     @Override
@@ -68,6 +77,9 @@ public abstract class Entity implements IEntity {
 
     // shader data loading
 
+    private void createModelMatrix(){
+
+    }
     protected void loadModelMatrix(){}
 
     protected void loadViewMatrix(ICamera camera){}
---
+++ src/scenes/DefaultScene.java
@@ -0,0 +1,10 @@
+package scenes;
+
+import core.scene.Scene;
+
+public class DefaultScene extends Scene {
+    @Override
+    protected void addItemsOnStartScene() {
+
+    }
+}
