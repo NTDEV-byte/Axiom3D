e0e1412d53c6b209e32804eef93859c36c263f58
NTDEV-byte <nassim.t998@gmail.com>
2022-08-19 16:32:32 +0200

    [Diffuse Light] lightFS Calculation

--- resources/shaders/global/light/lightFS.glsl
+++ resources/shaders/global/light/lightFS.glsl
@@ -1,9 +1,23 @@
 #version 400
 
 out vec4 color;
+
 in vec2 uvs;
+in vec3 tlsVec3;
+in vec3 transformedNormals;
+
+uniform vec3 lightColor;
+uniform sampler2D image;
 
 void main() {
 
-        color = vec4(0,1,0,1);
+        vec3 unitTLSVec3 = normalize(tlsVec3);
+        vec3 unitTN = normalize(transformedNormals);
+
+        float diffuseFactor = dot(unitTLSVec3,unitTN);
+        diffuseFactor = clamp(diffuseFactor,-1.0f , 1.0f);
+
+        vec3 diffuseColor = lightColor * diffuseFactor;
+
+        color = texture(image,uvs) * vec4(diffuseColor,1.0);
 }
