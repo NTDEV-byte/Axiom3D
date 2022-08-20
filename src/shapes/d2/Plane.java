515e9e245785f7d4b37b99716174e1ddf6345fea
NTDEV-byte <nassim.t998@gmail.com>
2022-08-20 13:45:31 +0200

    Plane generation

--- src/shapes/d2/Plane.java
+++ src/shapes/d2/Plane.java
@@ -1,27 +1,85 @@
 package shapes.d2;
 
+import core.scene.Scene;
 import core.scene.entity.Entity;
 import core.utils.shaders.Shader;
+import core.utils.vaos.Vertex;
 import core.utils.vaos.VertexArray;
+import org.lwjgl.util.vector.Vector2f;
 import org.lwjgl.util.vector.Vector3f;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Plane extends Entity {
 
     private static VertexArray mesh;
+    private int size = 800;
+    private int verticesCounter = 128;
 
     public Plane(Vector3f position) {
         super(mesh, new Shader("resources/shaders/global/raw/rawVS.glsl" , "resources/shaders/global/raw/rawFS.glsl"), position);
     }
 
+    public Plane(int size,int vertices,Vector3f position){
+        super(mesh, new Shader("resources/shaders/global/raw/rawVS.glsl" , "resources/shaders/global/raw/rawFS.glsl"), position);
+        this.size = size;
+        this.verticesCounter = vertices;
+    }
+
     @Override
     public void update() {
-
+        super.loadModelMatrix();
+        super.loadViewMatrix(Scene.MAIN_CAMERA);
     }
 
 
 
 
     private VertexArray generateMesh(){
+        List<Vertex> vertices = new ArrayList<Vertex>();
+        List<Integer> indices = new ArrayList<>();
+
+        int vertexIndex = 0;
+        for(int z = 0; z < verticesCounter; z++){
+             for(int x = 0; x < verticesCounter; x++){
+
+                 float vertexPX = (((float)x / (float)(verticesCounter  - 1))) * size;
+                 float vertexPY = 0.0f;
+                 float vertexPZ = (((float)z / (float)(verticesCounter  - 1))) * size;
+
+                 float vertexUVX = (((float)x / (float)(verticesCounter  - 1)));
+                 float vertexUVY = (((float)z / (float)(verticesCounter  - 1)));
+
+                 float vertexNormalX = 0.01f;
+                 float vertexNormalY = 0.01f;
+                 float vertexNormalZ = 0.02f;
+                 vertices.add(new Vertex(vertexIndex,new Vector3f(vertexPX,vertexPY,vertexPZ) , new Vector2f(vertexUVX,vertexUVY) , new Vector3f(vertexNormalX,vertexNormalY,vertexNormalZ));
+                 vertexIndex++;
+             }
+        }
+
+        // face collections
+        for(int z = 0; z < verticesCounter - 1; z++){
+            for(int x = 0; x < verticesCounter - 1; x++){
+
+                int topLeftVertex = (x + z * verticesCounter);
+                int topRightVertex = topLeftVertex + 1;
+                int bottomLeftVertex = (x + (z + 1) * verticesCounter);
+                int bottomRightVertex = bottomLeftVertex + 1;
+
+                indices.add(topLeftVertex);
+                indices.add(bottomLeftVertex);
+                indices.add(topRightVertex);
+
+                indices.add(topRightVertex);
+                indices.add(bottomLeftVertex);
+                indices.add(bottomRightVertex);
+
+            }
+        }
+
+        return new VertexArray(vertices, indices);
 
     }
 }
