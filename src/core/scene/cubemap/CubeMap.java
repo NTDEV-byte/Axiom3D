f5038a7f6a963dc9eae6db7a88b1041357eb2e57
NTDEV-byte <nassim.t998@gmail.com>
2022-08-26 23:21:33 +0200

    Right Face

--- src/core/scene/cubemap/CubeMap.java
+++ src/core/scene/cubemap/CubeMap.java
@@ -36,6 +36,13 @@ public class CubeMap extends Entity {
                     new Vertex(new Vector3f(-1.0f, 1.0f , 1.0f)),
                     new Vertex(new Vector3f(-1.0f, -1.0f , -1.0f)),
                     new Vertex(new Vector3f( -1.0f, -1.0f , 1.0f)),
+
+                    new Vertex(new Vector3f(-1.0f,1.0f,-1.0f)),
+                    new Vertex(new Vector3f(-1.0f , -1.0f,-1.0f)),
+                    new Vertex(new Vector3f(-1.0f, 1.0f , 1.0f)),       // Right face
+                    new Vertex(new Vector3f(-1.0f, 1.0f , 1.0f)),
+                    new Vertex(new Vector3f(-1.0f, -1.0f , -1.0f)),
+                    new Vertex(new Vector3f( -1.0f, -1.0f , 1.0f)),
             }));
     private static Texture texture = new CubeMapTexture(new String[]{
             "ocean/left.jpg", "ocean/right.jpg",
